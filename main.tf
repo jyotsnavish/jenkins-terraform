572473fe8871ca079922cb42d265fc5e7b00abb2
root <root@ip-172-31-80-194.ec2.internal>
2023-07-29 08:38:42 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-053b0d53c279acc90" 
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
