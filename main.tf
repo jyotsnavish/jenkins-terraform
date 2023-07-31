8a61cda6dae27edbbdfe4688a701da2aa860539f
jyotsnavish <jyotsana.vish98@gmail.com>
2023-07-31 07:52:02 +0000

    new branch added

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-053b0d53c279acc90" 
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "TF-Instance123"
   }
 }
